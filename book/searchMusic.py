78131c3a53376abc58f10d6201ddf3a2d5caa095
Jisun Park <fpdldk912@gmail.com>
2020-05-09 19:00:24 +0900

    use spotify web api to get artist's top10 playlist

--- book/searchMusic.py
+++ book/searchMusic.py
@@ -1,8 +1,37 @@
-import spotipy
+import os, sys, json
 from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import pprint
+from django.core.exceptions import ImproperlyConfigured
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-results = sp.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+# Get CLIENT_ID, CLIENT_SECRET from secrets.json
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets['spotify'][setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+client_id = get_secret("CLIENT_ID")
+client_secret = get_secret("CLIENT_SECRET")
+
+lz_uri = 'spotify:artist:6VuMaDnrHyPL1p4EHjYLi7' # Charlie Puth
+
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+results = sp.artist_top_tracks(lz_uri)
+
+# get top 10 tracks
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    print('audio    : ' + track['preview_url'])
+    print('cover art: ' + track['album']['images'][0]['url'])
+    print()
